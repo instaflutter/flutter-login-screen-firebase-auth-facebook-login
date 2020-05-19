1f87c8c44463b225d969814af84a25e3bf457077
mostafaAbdelazim <mzeemadev@gmail.com>
2020-05-19 03:33:35 +0200

    Flutter login init

--- FlutterLoginScreen/flutter_login_screen/lib/constants.dart
+++
@@ -1,8 +0,0 @@
-library constants;
-
-const FINISHED_ON_BOARDING = 'finishedOnBoarding';
-const COLOR_ACCENT = 0xFFfb634c;
-const COLOR_PRIMARY_DARK = 0xFF8a0000;
-const COLOR_PRIMARY = 0xFFC22F22;
-const FACEBOOK_BUTTON_COLOR = 0xFF415893;
-const USERS = 'users';
