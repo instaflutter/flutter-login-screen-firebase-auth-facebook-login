a99ac6dc4b54e40c652dca8dda17f0131c37fd6c
mostafaAbdelazim <mzeemadev@gmail.com>
2021-01-24 10:23:00 +0200

    bug fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_login_screen/model/User.dart';
 import 'package:flutter_login_screen/services/Authenticate.dart';
+import 'package:flutter_login_screen/services/helper.dart';
 import 'package:flutter_login_screen/ui/home/HomeScreen.dart';
-import 'file:///D:/FlutterFreebies2/FlutterLogin/lib/services/helper.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'constants.dart' as Constants;
