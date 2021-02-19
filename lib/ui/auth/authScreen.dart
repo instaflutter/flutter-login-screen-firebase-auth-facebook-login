f6e09db0bb7b5399f6bb033a455e596a1659fc8b
mostafaAbdelazim <mzeemadev@gmail.com>
2021-02-19 21:16:32 +0200

    code update and cleanup

--- lib/ui/auth/authScreen.dart
+++ lib/ui/auth/authScreen.dart
@@ -8,80 +8,79 @@ class AuthScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 200.0, bottom: 20.0),
-                child: Icon(
-                  Icons.phone_iphone,
-                  size: 150,
-                  color: Color(COLOR_PRIMARY),
-                ),
-              ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Icon(
+              Icons.phone_iphone,
+              size: 150,
+              color: Color(COLOR_PRIMARY),
             ),
-            Text(
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 16, top: 32, right: 16, bottom: 8),
+            child: Text(
               'Say Hello To Your New App!',
               textAlign: TextAlign.center,
               style: TextStyle(
                   color: Color(COLOR_PRIMARY),
-                  fontSize: 25.0,
+                  fontSize: 24.0,
                   fontWeight: FontWeight.bold),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'You\'ve just saved a week of development and headaches.',
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+            child: Text(
+              'You\'ve just saved a week of development and headaches.',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 40.0, left: 40.0, top: 40),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: RaisedButton(
+                color: Color(COLOR_PRIMARY),
+                child: Text(
+                  'Log In',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                textColor: Colors.white,
+                splashColor: Color(COLOR_PRIMARY),
+                onPressed: () => push(context, new LoginScreen()),
+                padding: EdgeInsets.only(top: 12, bottom: 12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: Color(COLOR_PRIMARY))),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0, left: 40.0, top: 20),
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: RaisedButton(
-                  color: Color(COLOR_PRIMARY),
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  textColor: Colors.white,
-                  splashColor: Color(COLOR_PRIMARY),
-                  onPressed: () {
-                    push(context, new LoginScreen());
-                  },
-                  padding: EdgeInsets.only(top: 12, bottom: 12),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      side: BorderSide(color: Color(COLOR_PRIMARY))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                right: 40.0, left: 40.0, top: 20, bottom: 20),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: FlatButton(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color(COLOR_PRIMARY)),
                 ),
+                onPressed: () => push(context, new SignUpScreen()),
+                padding: EdgeInsets.only(top: 12, bottom: 12),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                    side: BorderSide(color: Color(COLOR_PRIMARY))),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 40.0, left: 40.0, top: 20),
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: FlatButton(
-                  textColor: Colors.black54,
-                  child: Text(
-                    'Sign Up',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {
-                    push(context, new SignUpScreen());
-                  },
-                  padding: EdgeInsets.only(top: 12, bottom: 12),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      side: BorderSide(color: Colors.black54)),
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
